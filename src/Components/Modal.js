58a13839ae350ac7e30a1160bc58a8cdefd2b1c4
THT <thehelpfultipper@gmail.com>
2023-03-23 08:13:10 -0700

    Modal Card inclusion

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -5,8 +5,12 @@ const Modal = props => {
     return (
         <div className="modal_overlay">
             <Card>
-                <div className="modal_header"></div>
-                <div className="modal_body">Error message goes in here.</div>
+                <div className="modal_header">
+                    <p>Invalid input</p>
+                </div>
+                <div className="modal_body">
+                    <p>Error message goes in here.</p>
+                </div>
                 <Button type="button">Close</Button>
             </Card>
         </div>
